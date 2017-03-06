12b5b891b616129a0af68b825d655c1eb1abda7d
Suhash Mediboina <Suhash Mediboina>
2017-03-06 11:13:35 -0500

    add body parser package and mongo connectivity

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,11 +2,23 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCntrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello from the controller");
-  $http.get('/contactlist').then(function(response) {
-    console.log("I got the data I requested");
-    $scope.contactlist = response.data;
-  });
+    
+  var refresh = function() {
+      $http.get('/contactlist').then(function(response) {
+          console.log("I got the data I requested");
+          $scope.contactlist = response.data;
+          console.log(response.data);
+      });
+  };
+    
+ refresh();
+    
   $scope.addContact = function() {
      console.log($scope.contact); 
+     $http.post('/contactlist', $scope.contact).then(function(response) {
+         console.log(response);
+         refresh();
+         $scope.contact = "";
+     })
   };
 }]);
--- server.js
+++ server.js
@@ -2,14 +2,25 @@ var express = require('express');
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
 app.get('/contactlist', function(req, res) {
    console.log("I received a GET request") ;
    db.contactlist.find(function(err, docs) {
        console.log(docs);
        res.json(docs);
    });
+});
+
+app.post('/contactlist', function(req, res) {
+    debugger
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc);
+    });
 });
 
 app.listen(5000);
